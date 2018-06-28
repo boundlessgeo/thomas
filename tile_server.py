e997115758febdd0c1e35598ffb2ee64d727e81c
tingold <tingold@boundlessgeo.com>
2018-06-28 18:29:42 -0400

    fixed requirements

--- tile_server.py
+++ tile_server.py
@@ -27,7 +27,7 @@ class InferenceConfig(BuildingConfig):
         # Set batch size to 1 since we'll be running inference on
         # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
     GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
+    IMAGES_PER_GPU = 8
 
 
 config = InferenceConfig()
