fb01648a46d15318c7fee911db2044151f1b80d7
tingold <tingold@boundlessgeo.com>
2018-06-28 21:26:38 -0400

    fixed requirements

--- tile_server.py
+++ tile_server.py
@@ -21,12 +21,19 @@ if (PLATFORM.startswith("Darwin")):
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 WEIGHTS = os.path.join(ROOT_DIR, 'mask_rcnn_buildings_1.h5')
 
+
 class InferenceConfig(BuildingConfig):
     IMAGES_PER_GPU = 1
 
+
 config = InferenceConfig()
-model = modellib.MaskRCNN(mode="inference", config=config, model_dir=MODEL_DIR)
+
+global model
+model = modellib.MaskRCNN(mode="inference", config=config,
+                          model_dir=MODEL_DIR)
 model.load_weights(WEIGHTS, by_name=True)
+global graph
+graph = tf.get_default_graph()
 
 
 @app.route('/', methods=['GET'])
@@ -37,24 +44,24 @@ def index():
 @app.route('/tiles/<int:z>/<int:x>/<int:y>', methods=['GET'])
 def tile(z, x, y):
     request_start_time = time.time()
+    with graph.as_default():
     # if z != 18:
     # only service level 18 for now
     #    abort(404)
-    url = "https://b.tiles.mapbox.com/v4/mapbox.satellite/{}/{}/{}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NDg1bDA1cjYzM280NHJ5NzlvNDMifQ.d6e-nNyBDtmQCVwVNivz7A".format(
-        z, x, y)
-    image = skimage.io.imread(url)
-    r = model.detect([image], verbose=1)[0]
-    # output = Image.new('RGBA',(256,256),(0,0,0,0))
-    for i in range(len(r['rois'])):
-        image = visualize.draw_box(image, r['rois'][i], (255, 0, 0))
-        # image = visualize.apply_mask(image, r['masks'][i], (255, 0, 0))
-    output = Image.fromarray(image, 'RGB')
-    byte_io = BytesIO()
-    output.save(byte_io, 'PNG')
-    byte_io.seek(0)
-    request_end_time = time.time();
-    print(request_end_time - request_start_time)
-    return send_file(byte_io, mimetype='image/png')
+        url = "https://b.tiles.mapbox.com/v4/mapbox.satellite/{}/{}/{}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NDg1bDA1cjYzM280NHJ5NzlvNDMifQ.d6e-nNyBDtmQCVwVNivz7A".format(z, x, y)
+        image = skimage.io.imread(url)
+        r = model.detect([image], verbose=1)[0]
+        # output = Image.new('RGBA',(256,256),(0,0,0,0))
+        for i in range(len(r['rois'])):
+            image = visualize.draw_box(image, r['rois'][i], (255, 0, 0))
+            # image = visualize.apply_mask(image, r['masks'][i], (255, 0, 0))
+        output = Image.fromarray(image, 'RGB')
+        byte_io = BytesIO()
+        output.save(byte_io, 'PNG')
+        byte_io.seek(0)
+        request_end_time = time.time();
+        print(request_end_time - request_start_time)
+        return send_file(byte_io, mimetype='image/png')
 
 
 if __name__ == '__main__':
