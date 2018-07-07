fb611f134a7a2b8299230310361eab934120fc84
tingold <tingold@boundlessgeo.com>
2018-07-07 18:31:53 -0400

    tweaking training params

--- objects/osm.py
+++ objects/osm.py
@@ -47,7 +47,7 @@ class BuildingConfig(Config):
     # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
     # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
     GPU_COUNT = 1
-    IMAGES_PER_GPU = 8
+    IMAGES_PER_GPU = 32
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 3  # background + 3 classes
@@ -62,13 +62,13 @@ class BuildingConfig(Config):
 
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 32
+    TRAIN_ROIS_PER_IMAGE = 64
 
     # Use a small epoch since the data is simple
-    STEPS_PER_EPOCH = 100
+    STEPS_PER_EPOCH = 1000
 
     # use small validation steps since the epoch is small
-    VALIDATION_STEPS = 10
+    VALIDATION_STEPS = 100
     USE_MINI_MASK = False
 
 
