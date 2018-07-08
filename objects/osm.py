9d253464e371cb6c3fd7de82d25a359a703ae28b
tingold <tingold@boundlessgeo.com>
2018-07-07 20:13:06 -0400

    tweaking training params

--- objects/osm.py
+++ objects/osm.py
@@ -62,7 +62,7 @@ class BuildingConfig(Config):
 
     # Reduce training ROIs per image because the images are small and have
     # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
-    TRAIN_ROIS_PER_IMAGE = 64
+    TRAIN_ROIS_PER_IMAGE = 32
 
     # Use a small epoch since the data is simple
     STEPS_PER_EPOCH = 1000
